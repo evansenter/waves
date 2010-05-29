a1fb074c121a9c61d71c172a666565695efa3dc0
Evan Senter <evansenter@gmail.com>
2010-05-29 10:21:03 -0400

    last.rb module

---
+++ lib/last.rb
@@ -0,0 +1,29 @@
+module Last
+  class << self
+    def similar_artists_for(name)
+      get_similar_artists_for(name).css("similarartists artist").select do |artist|
+        extract(:mbid, artist).present?
+      end.map do |artist|
+        {
+          :name  => extract(:name,  artist),
+          :mbid  => extract(:mbid,  artist),
+          :match => extract(:match, artist)
+        }
+      end
+    end
+    
+    private
+    
+    def get_similar_artists_for(name)
+      Nokogiri::XML(open(similar_artist_url(name)))
+    end
+    
+    def similar_artist_url(name)
+      "http://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&artist=#{URI.encode(name)}&api_key=#{LastApi::KEY}"
+    end
+    
+    def extract(attribute, container)
+      container.css("#{attribute}").first.content
+    end
+  end
+end
